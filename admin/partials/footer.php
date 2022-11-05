9aa74df3a4c544026ab8747d20d47607da36e768
Gracie Sharma <graciesharma0@gmail.com>
2022-11-06 00:24:17 +0545

    design update

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,10 +1,12 @@
 <!-- Footer Section Starts -->
 <div class="footer">
             <div class="wrapper">
-                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Gracie Sharma </a></p>
+                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Gracie Sharma</a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
 
     </body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </html>
